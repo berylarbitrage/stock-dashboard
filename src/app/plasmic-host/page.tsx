e14acda9f6314408bc6d5cf01422bfc813e92899
berylarbitrage <beryl.superiorbrandscorp@gmail.com>
2026-02-06 01:38:39 +0000

    fix: add minimal plasmic host

--- src/app/plasmic-host/page.tsx
+++ src/app/plasmic-host/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
-import { PLASMIC } from "../../../plasmic-init";
 
 export default function PlasmicHostPage() {
-  return PLASMIC && <PlasmicCanvasHost />;
+  return <PlasmicCanvasHost />;
 }
